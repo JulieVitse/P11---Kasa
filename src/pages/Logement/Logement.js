41fef953b316e25191bb5a229cb68c2e7879a149
Julie V <julievitse91@gmail.com>
2022-09-26 21:09:49 +0200

    + titles and tags

--- src/pages/Logement/Logement.js
+++ src/pages/Logement/Logement.js
@@ -1,4 +1,5 @@
 import Gallery from '../../components/Gallery/Gallery'
+import Tag from '../../components/Tag/Tag'
 import { useParams } from 'react-router-dom'
 import styles from './Logement.module.scss'
 
@@ -10,6 +11,13 @@ function Logement({ logements }) {
   return (
     <section className={styles.container}>
       <Gallery pictures={logement.pictures} title={logement.title} />
+      <h2 className={styles.title}>{logement.title}</h2>
+      <h3 className={styles.location}>{logement.location}</h3>
+      <div className={styles.tags_wrapper}>
+        {logement.tags.map((tag, index) => (
+          <Tag key={index} tags={tag} />
+        ))}
+      </div>
     </section>
   )
 }
