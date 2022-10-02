3c5dd08e5bd6e891d017fdcc0d31edfd55febd91
Julie V <julievitse91@gmail.com>
2022-10-02 14:31:43 +0200

    added condition on logement page to check for correct id

--- src/pages/Logement/Logement.js
+++ src/pages/Logement/Logement.js
@@ -2,6 +2,7 @@ import Gallery from 'components/Gallery/Gallery'
 import Tag from 'components/Tag/Tag'
 import Rating from 'components/Rating/Rating'
 import Collapse from 'components/Collapse/Collapse'
+import Error from 'pages/Error/Error'
 import { useParams } from 'react-router-dom'
 import styles from './Logement.module.scss'
 
@@ -9,34 +10,38 @@ function Logement({ logements }) {
   const { id } = useParams()
   const logement = logements.find((logement) => logement.id === id)
 
-  return (
-    <section className={styles.container}>
-      <Gallery pictures={logement.pictures} title={logement.title} />
-      <h2 className={styles.title}>{logement.title}</h2>
-      <h3 className={styles.location}>{logement.location}</h3>
-      <div className={styles.tags_wrapper}>
-        {logement.tags.map((tag, index) => (
-          <Tag key={index} tags={tag} />
-        ))}
-      </div>
-
-      <div className={styles.informations}>
-        <Rating ratingValue={logement.rating} />
-        <div className={styles.host}>
-          <p className={styles.name}>{logement.host.name}</p>
-          <img
-            src={logement.host.picture}
-            alt={logement.host.name}
-            className={styles.picture}
-          />
+  return logement ? (
+    <main>
+      <section className={styles.container}>
+        <Gallery pictures={logement.pictures} title={logement.title} />
+        <h2 className={styles.title}>{logement.title}</h2>
+        <h3 className={styles.location}>{logement.location}</h3>
+        <div className={styles.tags_wrapper}>
+          {logement.tags.map((tag, index) => (
+            <Tag key={index} tags={tag} />
+          ))}
         </div>
-      </div>
 
-      <div className={styles.wrapper}>
-        <Collapse title="Description" content={logement.description} />
-        <Collapse title="Équipements" content={logement.equipments} />
-      </div>
-    </section>
+        <div className={styles.informations}>
+          <Rating ratingValue={logement.rating} />
+          <div className={styles.host}>
+            <p className={styles.name}>{logement.host.name}</p>
+            <img
+              src={logement.host.picture}
+              alt={logement.host.name}
+              className={styles.picture}
+            />
+          </div>
+        </div>
+
+        <div className={styles.wrapper}>
+          <Collapse title="Description" content={logement.description} />
+          <Collapse title="Équipements" content={logement.equipments} />
+        </div>
+      </section>
+    </main>
+  ) : (
+    <Error />
   )
 }
 
